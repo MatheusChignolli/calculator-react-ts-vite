97ce9082085c2298bdf4a14818d5e25c2148efb2
Matheus Chignolli <matheuschignolli@gmail.com>
2023-06-17 19:55:14 -0300

    create calculator logics

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,219 @@
 import { useState } from "react";
 import "./App.css";
 
-const initialValue = "0";
-const initialOperation = "";
+const initialValues = {
+  old: "0",
+  new: "0",
+};
+const initialOperation = null;
+
+enum Operations {
+  sum = "sum",
+  minus = "minus",
+  divide = "divide",
+  multiply = "multiply",
+}
 
 function App() {
-  const [value, setValue] = useState(initialValue);
-  const [operation, setOperation] = useState(initialOperation);
+  const [values, setValues] = useState(initialValues);
+  const [operation, setOperation] = useState<Operations | null>(
+    initialOperation
+  );
 
   const clear = () => {
-    setValue(initialValue);
+    setValues(initialValues);
     setOperation(initialOperation);
   };
 
   const positiveNegative = () => {
-    setValue((prevState) => {
-      if (prevState.indexOf("-") === 0) {
-        return prevState.slice(1);
-      } else {
-        return `-${prevState}`;
+    setValues((prevState) => {
+      if (prevState.new.indexOf("-") === 0) {
+        return {
+          ...prevState,
+          new: prevState.new.replace("-", ""),
+        };
       }
+
+      return {
+        ...prevState,
+        new: `-${prevState.new}`,
+      };
     });
   };
 
   const percentage = () => {
-    setValue((prevState) => {
-      const value = parseFloat(prevState);
-      return String(value / 100);
+    setValues((prevState) => {
+      const value = parseFloat(prevState.new);
+      return {
+        ...prevState,
+        new: String(value / 100),
+      };
     });
   };
 
-  const addComa = () => {
-    setValue((prevState) => {
-      if (prevState.indexOf(",") === -1) {
-        return `${prevState},`;
-      }
-
-      return prevState;
-    });
+  const addDot = () => {
+    if (values.new.indexOf(".") === -1) {
+      setValues((prevState) => ({ ...prevState, new: `${prevState.new}.` }));
+    }
   };
 
   const setNumber = (number: string) => {
-    setValue((prevState) => {
-      if (prevState === "0") {
-        return number;
+    setValues((prevState) => {
+      if (["0", "-0"].includes(prevState.new)) {
+        return {
+          ...prevState,
+          new: prevState.new.includes("-") ? `-${number}` : number,
+        };
       } else {
-        return `${prevState}${number}`;
+        return {
+          ...prevState,
+          new: `${prevState.new}${number}`,
+        };
       }
     });
   };
 
-  const calculate = () => {
-    console.log("oi");
+  const chooseOperation = (operation: Operations) => {
+    setOperation(operation);
+    setValues((prevState) => ({
+      new: "0",
+      old: prevState.new,
+    }));
   };
+
+  const sum = (a: string, b: string) => {
+    return String(parseFloat(a) + parseFloat(b));
+  };
+
+  const minus = (a: string, b: string) => {
+    return String(parseFloat(a) - parseFloat(b));
+  };
+
+  const multiply = (a: string, b: string) => {
+    return String(parseFloat(a) * parseFloat(b));
+  };
+
+  const divide = (a: string, b: string) => {
+    return String(parseFloat(a) / parseFloat(b));
+  };
+
+  const operations = {
+    sum,
+    minus,
+    multiply,
+    divide,
+  };
+
+  const calculate = () => {
+    if (operation) {
+      setValues((prevState) => {
+        return {
+          old: "0",
+          new: operations[operation](prevState.old, prevState.new),
+        };
+      });
+      setOperation(initialOperation);
+    }
+  };
+
+  const buttons = [
+    {
+      label: "AC",
+      className: "grey",
+      onClick: clear,
+    },
+    {
+      label: "+/-",
+      className: "grey",
+      onClick: positiveNegative,
+    },
+    {
+      label: "%",
+      className: "grey",
+      onClick: percentage,
+    },
+    {
+      label: "/",
+      className: "contrast",
+      onClick: () => chooseOperation(Operations.divide),
+    },
+    {
+      label: "7",
+      onClick: () => setNumber("7"),
+    },
+    {
+      label: "8",
+      onClick: () => setNumber("8"),
+    },
+    {
+      label: "9",
+      onClick: () => setNumber("9"),
+    },
+    {
+      label: "x",
+      className: "contrast",
+      onClick: () => chooseOperation(Operations.multiply),
+    },
+    {
+      label: "4",
+      onClick: () => setNumber("4"),
+    },
+    {
+      label: "5",
+      onClick: () => setNumber("5"),
+    },
+    {
+      label: "6",
+      onClick: () => setNumber("6"),
+    },
+    {
+      label: "-",
+      className: "contrast",
+      onClick: () => chooseOperation(Operations.minus),
+    },
+    {
+      label: "1",
+      onClick: () => setNumber("1"),
+    },
+    {
+      label: "2",
+      onClick: () => setNumber("2"),
+    },
+    {
+      label: "3",
+      onClick: () => setNumber("3"),
+    },
+    {
+      label: "+",
+      className: "contrast",
+      onClick: () => chooseOperation(Operations.sum),
+    },
+    {
+      label: "0",
+      className: "zero",
+      onClick: () => setNumber("0"),
+    },
+    {
+      label: ",",
+      onClick: addDot,
+    },
+    {
+      label: "=",
+      className: "contrast",
+      onClick: calculate,
+    },
+  ];
 
   return (
     <main>
-      <div className="result">{value}</div>
-      <button className="grey" onClick={clear}>
-        AC
-      </button>
-      <button className="grey" onClick={positiveNegative}>
-        +/-
-      </button>
-      <button className="grey" onClick={percentage}>
-        %
-      </button>
-      <button className=" contrast" onClick={() => setOperation("divide")}>
-        /
-      </button>
-      <button onClick={() => setNumber("7")}>7</button>
-      <button onClick={() => setNumber("8")}>8</button>
-      <button onClick={() => setNumber("9")}>9</button>
-      <button className="contrast" onClick={() => setOperation("multiply")}>
-        x
-      </button>
-      <button onClick={() => setNumber("4")}>4</button>
-      <button onClick={() => setNumber("5")}>5</button>
-      <button onClick={() => setNumber("6")}>6</button>
-      <button className="contrast" onClick={() => setOperation("minus")}>
-        -
-      </button>
-      <button onClick={() => setNumber("1")}>1</button>
-      <button onClick={() => setNumber("2")}>2</button>
-      <button onClick={() => setNumber("3")}>3</button>
-      <button className="contrast" onClick={() => setOperation("sum")}>
-        +
-      </button>
-      <button className="zero" onClick={() => setNumber("0")}>
-        0
-      </button>
-      <button onClick={addComa}>,</button>
-      <button className="contrast" onClick={calculate}>
-        =
-      </button>
+      <div className="result">
+        {typeof values.new === "string" ? values.new : "Error!"}
+      </div>
+      {buttons.map(({ label, ...buttonProps }, index) => (
+        <button key={`button-${index}`} {...buttonProps}>
+          {label}
+        </button>
+      ))}
     </main>
   );
 }
